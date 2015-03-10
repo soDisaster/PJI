6192f50cc69dc9540a2c90f27ad9627015f9be6b
Anne-Sophie Saint-Omer <anne.sophie.saint.omer28@gmail.com>
2015-03-10 14:06:48 +0100

    Bon plus de bêtises, projet sous GitHub

---
+++ BDD&ListeD/listeD.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+ 
+</head>
+<body>
+	<form>
+			<?php
+				include('listeDeroulante.php'); 
+		 		listeD();
+	   		?>
+	</form>
+</body>
+</html>
+
+
